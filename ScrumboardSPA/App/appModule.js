8c9690901e7f16cfc42eacdb9f37283c6623fc05
timbussmann <notthemail@youarelooking.for>
2013-05-10 16:51:52 +0200

    Added dropzone directive to module

--- ScrumboardSPA/App/appModule.js
+++ ScrumboardSPA/App/appModule.js
@@ -14,3 +14,22 @@
 
     $locationProvider.html5Mode(true);
 });
+
+app.directive('dropzone', function () {
+    return function (scope, element, attrs) {
+        element[0].addEventListener('dragover', function(e) {
+            e.preventDefault && e.preventDefault();
+
+            e.dataTransfer.dropEffect = 'move';
+
+            return false;
+        }, false);
+
+        element[0].addEventListener('dragenter', function(e) {
+            element[0].classList.add('dropzoneDragOver');
+        }, false);
+        element[0].addEventListener('dragleave', function(e) {
+            this.classList.remove('dropzoneDragOver');
+        }, false);
+    };
+});
