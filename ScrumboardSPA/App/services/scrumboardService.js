3fb91ffd14d26de2bc8502d6cb3051c650ea9c80
timbussmann <notthemail@youarelooking.for>
2013-05-18 12:51:02 +0200

    Fixed service instantiation for scrumboardService
    See difference between factory and service module directives. Service
    expectes a constructor function instead of module pattern style function
    (see factory).

--- ScrumboardSPA/App/services/scrumboardService.js
+++ ScrumboardSPA/App/services/scrumboardService.js
@@ -1,27 +1,26 @@
-﻿app.service('scrumboardService', function ($http) {
-
-    return {
-        getStates: function(callback) {
-            $http.get('/api/stories/states', {
-                cache:  true
-            }).success(callback);
-        },
-        getStories: function(callback) {
-            $http.get('/api/stories').success(callback);
-        },
-        getStory: function(storyId, callback) {
-            $http.get('/api/story/' + storyId).success(callback);
-        },
-        setStoryState: function (story, state, successCallback, errorCallback) {
-            $http.put('/api/story/' + story.Id + '/state/' + state,
-                '"' + story.Etag + '"') // WebAPI requires single primitive datatypes as a string and not as json object
-                .success(successCallback)
-                .error(errorCallback);
-        },
-        createStory: function(story, successCallback, errorCallback) {
-            $http.post('/api/story', story)
-                .success(successCallback)
-                .error(errorCallback);
-        }
+﻿app.service('scrumboardService', function($http) {
+    // angular services expect a constructor function. for a module pattern style way, look at angular factory
+    
+    this.getStates = function(callback) {
+        $http.get('/api/stories/states', {
+            cache: true
+        }).success(callback);
+    };
+    this.getStories = function(callback) {
+        $http.get('/api/stories').success(callback);
+    };
+    this.getStory = function(storyId, callback) {
+        $http.get('/api/story/' + storyId).success(callback);
+    };
+    this.setStoryState = function(story, state, successCallback, errorCallback) {
+        $http.put('/api/story/' + story.Id + '/state/' + state,
+            '"' + story.Etag + '"') // WebAPI requires single primitive datatypes as a string and not as json object
+            .success(successCallback)
+            .error(errorCallback);
+    };
+    this.createStory = function(story, successCallback, errorCallback) {
+        $http.post('/api/story', story)
+            .success(successCallback)
+            .error(errorCallback);
     };
 });
