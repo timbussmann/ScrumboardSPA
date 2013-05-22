d42bb35535ebf8a5820db25a95533dc3a8159335
timbussmann <tim.bussmann@bbv.ch>
2013-05-22 18:20:40 +0200

    Added missing changes

---
+++ ScrumboardSPA.Data/Conflicts/StoryUpdateConflict.cs
@@ -0,0 +1,13 @@
+﻿namespace ScrumboardSPA.Data.Conflicts
+{
+    using ScrumboardSPA.Data.Story;
+
+    class StoryUpdateConflict
+    {
+        public string ConflictId { get; set; }
+
+        public UserStory Original { get; set; }
+
+        public UserStory Requested { get; set; }
+    }
+}
