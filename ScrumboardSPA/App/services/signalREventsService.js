59fbc3c34bfd0ec39cb09dba8f41901e35cdd859
timbussmann <notthemail@youarelooking.for>
2013-05-28 18:04:31 +0200

    prevent signalR exceptions when in offline mode

--- ScrumboardSPA/App/services/signalREventsService.js
+++ ScrumboardSPA/App/services/signalREventsService.js
@@ -1,20 +1,22 @@
 ﻿app.service('signalREventsService',
-    ['$rootScope', function ($rootScope) {
-    
-    // Proxy created on the fly          
-    var hub = $.connection.storyHub;
+    ['$rootScope', function($rootScope) {
 
-    hub.client.updateStory = function (updatedStory) {
-        // execute within apply because signalR does not run within angular scopes
-        $rootScope.$apply(function() {
-            $rootScope.$broadcast('StoryChanged', updatedStory);
-        });
-    };
-    
-    hub.client.createStory = function (createdStory) {
-        $rootScope.$broadcast('CreateSuccessful', createdStory);
-    };
+        // Proxy created on the fly          
+        var hub = $.connection.storyHub;
 
-    // Start the connection
-    $.connection.hub.start();
-}]);
+        if (hub) {
+            hub.client.updateStory = function(updatedStory) {
+                // execute within apply because signalR does not run within angular scopes
+                $rootScope.$apply(function() {
+                    $rootScope.$broadcast('StoryChanged', updatedStory);
+                });
+            };
+
+            hub.client.createStory = function(createdStory) {
+                $rootScope.$broadcast('CreateSuccessful', createdStory);
+            };
+            
+            // Start the connection
+            $.connection.hub.start();
+        }
+    }]);
