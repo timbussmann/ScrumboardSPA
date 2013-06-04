1b8291a4fe6eb036d8dd9f1de83469cc44742c9e
Meinrad Andermatt <meinrad.andermatt@bbv.ch>
2013-06-04 17:32:41 +0200

    added integrate Script

---
+++ Scripts/integrate_javascript.bat
@@ -0,0 +1,16 @@
+@echo off
+set resultDir=..\results
+if not exist %resultDir% mkdir %resultDir%
+
+..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log javascript-unit-tests 
+IF ERRORLEVEL 1 GOTO Failed
+
+echo "compilation and unit testing completed. Log file and unit-tests results are stored in %resultDir%"
+GOTO End
+
+
+:Failed
+echo "Failed"
+
+:End
+pause
