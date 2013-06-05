78616fac0f14fd94de60de28fde307a800c7c2cf
Meinrad Andermatt <meinrad.andermatt@bbv.ch>
2013-06-05 13:03:21 +0200

    modified integrate added errorhandling

--- Scripts/integrate_javascript.bat
+++ Scripts/integrate_javascript.bat
@@ -3,8 +3,11 @@ set resultDir=..\results
 if not exist %resultDir% mkdir %resultDir%
 
 ..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log integration-build 
+IF ERRORLEVEL 1 GOTO Failed
 ..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log unit-tests 
+IF ERRORLEVEL 1 GOTO Failed
 ..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log javascript-unit-tests 
+IF ERRORLEVEL 1 GOTO Failed
 ..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log javascript-code-coverage 
 IF ERRORLEVEL 1 GOTO Failed
 
