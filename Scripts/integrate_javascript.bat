3832e7cb3ae36f345e81ba247e70d0fd7ec362f2
Meinrad Andermatt <meinrad.andermatt@bbv.ch>
2013-06-05 10:29:21 +0200

    added code coverage to integrate

--- Scripts/integrate_javascript.bat
+++ Scripts/integrate_javascript.bat
@@ -3,6 +3,7 @@ set resultDir=..\results
 if not exist %resultDir% mkdir %resultDir%
 
 ..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log javascript-unit-tests 
+..\tools\NAnt\NAnt -buildfile:nant.build -l:%resultDir%\TDDJavascript-Nant-jscript-tests.log javascript-code-coverage 
 IF ERRORLEVEL 1 GOTO Failed
 
 echo "compilation and unit testing completed. Log file and unit-tests results are stored in %resultDir%"
